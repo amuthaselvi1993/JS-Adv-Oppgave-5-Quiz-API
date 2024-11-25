e380e08dc478d66ac8adf93d25bf653a310a9ed3
amutha-kodehode <amutha.kodehode@gmail.com>
2024-11-25 23:43:57 +0100

    Minor  fix in decoding

--- app.js
+++ app.js
@@ -103,11 +103,11 @@ function processQuestions(data) {
 }
 function decodeHtmlEntities(input) {
   return input
-    .replace(/&#039;/, "'")
-    .replace(/&amp;/, "&")
-    .replace(/&quot;/, '"')
-    .replace(/&rsquo;/, "'")
-    .replace(/&shy;/, "-");
+    .replaceAll(/&#039;/, "'")
+    .replaceAll(/&amp;/, "&")
+    .replaceAll(/&quot;/, '"')
+    .replaceAll(/&rsquo;/, "'")
+    .replaceAll(/&shy;/, "-");
 }
 function removeOldQuestions() {
   while (questionPanel.firstChild)
